78708c788a648d2a5ec6f54a9dd3972e3701e61b
Venus Chau <vc2000@wildcats.unh.edu>
2018-11-14 11:22:25 -0500

    data need to be clean

---
+++ analyst.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+# nh towns and cities
+tc_list=[]
+with open('nh_town_city.txt') as file:
+    for line in file:
+        tc_list.append(line.replace('\n','').lower())
+
+
+df = pd.read_csv("original-houses.csv")
+
+df["City"]=df["City"].apply(str)
+df["City"]=df["City"].str.lower()
+df["City"]=df["City"].str.replace(',nh','').replace('nh','')
+
+df["Price"] = df["Price"].str.replace('$','')
+
+df["Title"] = df["Title"].str.lower()
+df["Title"] = df["Title"].str.replace('w/','with ').replace('/wk',' per week')
+
+df["bedroom"]= df["bedroom"].str.replace('br','')
+
+df["ft"]= df["ft"].str.replace('ft2','')
+
+
+
+print(df.head(100))
+
---
+++ crawl-craigslist.py
@@ -0,0 +1,56 @@
+import requests
+import pandas
+from bs4 import BeautifulSoup
+
+big_lis = []
+
+i=0
+while i < 3000:
+    try:
+        if i == 0:
+            url = "https://nh.craigslist.org/search/apa"
+        else:
+            url = "https://nh.craigslist.org/search/apa?s={}".format(i)
+
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+
+        
+
+        all = soup.find_all("li", {"class": "result-row"})
+
+        for items in all:
+            d = {}
+            d["Title"] = items.find("a", {"class": "result-title hdrlnk"}).text
+            try:
+                d["City"] = (
+                    items.find("span", {"class": "result-hood"})
+                    .text.replace(" ", "")
+                    .replace("(", "")
+                    .replace(")", "")
+                )
+            except:
+                print(None)
+            try:
+                det = (
+                    items.find("span", {"class": "housing"})
+                    .text.replace("\n", "")
+                    .replace(" ", "")
+                    .split("-")
+                )
+                d["bedroom"] = det[0]
+                d["ft"] = det[1]
+                # d["Detail"] = detail
+            except:
+                print(None)
+            try:
+                d["Price"] = items.find("span", {"class": "result-price"}).text
+            except:
+                print(None)
+            big_lis.append(d)
+    except:
+        print("stop")
+    i += 120
+
+df = pandas.DataFrame(big_lis)
+df.to_csv("original-houses.csv")
