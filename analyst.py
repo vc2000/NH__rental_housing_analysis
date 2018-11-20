220e8295381f75945b4f2d5f0d4fd8f98af9b695
vc2000 <vc2000@wildcats.unh.edu>
2018-11-20 13:05:00 -0500

    ad num per city

--- analyst.py
+++ analyst.py
@@ -5,30 +5,36 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 
 df = pd.read_csv("halfclean(11).csv", encoding="ISO-8859-1")
-print(df.head(10))
+#print(df.head(10))
+
+"""
+    number of advertisment per town or city
+"""
+#df.groupby('City').size().to_csv('ad_by_city.csv')
+
 
 """
 price per city or town
 """
-avg_price_city = df.groupby(["City"])["Price"].mean()
+# avg_price_city = df.groupby(["City"])["Price"].mean()
 
-avg_price_city.to_csv("avg_price_city.csv", encoding="utf-8")
+# avg_price_city.to_csv("avg_price_city.csv", encoding="utf-8")
 
 
 """
     Median price for each city
 """
-median_price_city = df.groupby(["City"])["Price"].median()
-print(median_price_city)
-median_price_city.to_csv("median_price_city.csv", encoding="utf-8")
+# median_price_city = df.groupby(["City"])["Price"].median()
+# print(median_price_city)
+# median_price_city.to_csv("median_price_city.csv", encoding="utf-8")
 
 """
     Average price per ft for each city
     *** use new dataset that remove null "ft"
 """
 
-avg_price_per_ft_in_city = df.groupby(["City"])["price_per_ft"].mean()
-avg_price_per_ft_in_city.to_csv("avg_price_per_ft_in_city.csv", encoding="utf-8")
+# avg_price_per_ft_in_city = df.groupby(["City"])["price_per_ft"].mean()
+# avg_price_per_ft_in_city.to_csv("avg_price_per_ft_in_city.csv", encoding="utf-8")
 
 
 """
@@ -36,14 +42,14 @@ avg_price_per_ft_in_city.to_csv("avg_price_per_ft_in_city.csv", encoding="utf-8"
     *** use new dataset that remove null "bedroom"
 """
 
-avg_price_per_ft_in_city = df.groupby(["City"])["price_per_br"].mean()
-avg_price_per_ft_in_city.to_csv("avg_price_per_bedroom_in_city.csv", encoding="utf-8")
+# avg_price_per_ft_in_city = df.groupby(["City"])["price_per_br"].mean()
+# avg_price_per_ft_in_city.to_csv("avg_price_per_bedroom_in_city.csv", encoding="utf-8")
 
 """
     Number of garage that apartment offer
 """
 
-num_garage = str(df.Title.str.count('garage').sum())
+# num_garage = str(df.Title.str.count('garage').sum())
 
-with open("num_garage.txt", "w") as text_file:
-    print(f"Number of garage that apartment offer : {num_garage}", file=text_file)
+# with open("num_garage.txt", "w") as text_file:
+#    print(f"Number of garage that apartment offer : {num_garage}", file=text_file)
